307c98def8af4be00862c2c3d4af0144281feaf4
0BastAV0 <83418922+0BastAV0@users.noreply.github.com>
2021-05-06 22:13:53 -0600

    First Push

---
+++ Nier/Assets/Scripts/formsNextpage.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class formsNextpage : MonoBehaviour
+{
+    public GameObject currentScreen;
+    public GameObject nextScreen;
+
+    public Text warning;
+
+    public Text users;
+    public Text names;
+    public Text lastname;
+    public Text password;
+
+    public bool continueToNextPage;
+
+    public void Update()
+    {
+        InformationVal();
+    }
+    public void InformationVal()
+    {
+        if (names.text != "" && lastname.text != "" && users.text != "" && password.text != "")
+        {
+            continueToNextPage = true;
+        }
+    }
+
+    public void nextPage()
+    {
+        if (continueToNextPage)
+        {
+            currentScreen.SetActive(false);
+            nextScreen.SetActive(true);
+        }
+        else
+        {
+            warning.text = "You have forgoten some information";
+        }
+    }
+}
---
+++ Nier/Assets/Scripts/nextPage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nextPage : MonoBehaviour
+{
+    
+   
+        public GameObject thisPage;
+        public GameObject nextoPage;
+
+
+        public void ChangeScreen()
+        {
+            thisPage.SetActive(false);
+            nextoPage.SetActive(true);
+        }
+    
+}
